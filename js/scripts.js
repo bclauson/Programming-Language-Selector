249c38524db8ee5d6a01927a419216d8fc6af80d
bclauson <clauson.branden@gmail.com>
2021-11-06 23:52:24 -0700

    added final questions with same format as previous, along with submit result and restart button

--- js/scripts.js
+++ js/scripts.js
@@ -12,8 +12,6 @@ $(document).ready(function() {
         cSharp = cSharp + 1
       } else if (programming === "java") {
         javaScript = javaScript + 1
-      } else if (programming === "python") {
-        python = python + 1
       }
       console.log(python)
       console.log(javaScript)
@@ -31,9 +29,7 @@ $(document).ready(function() {
           python = python + 1
         } else if (programming === "cSharp") {
           cSharp = cSharp + 1
-        } else if (programming === "java") {
-          javaScript = javaScript + 1
-        }
+        } 
         console.log(python)
         console.log(javaScript)
         console.log(cSharp)
@@ -52,13 +48,72 @@ $(document).ready(function() {
             javaScript = javaScript + 1
           } else if (programming === "python") {
             python = python + 1
-          } else if (programming === "cSharp") {
-            cSharp = cSharp + 1
           }
           console.log(python)
           console.log(javaScript)
           console.log(cSharp)
           $(".questionThree").toggle();
-          
+          $(".questionFour").toggle();
+
         });
+
+        $("#submitFour").click(function() {
+          event.preventDefault();
+            const programming = $("input:radio[name=programming]:checked").val();
+            
+            if (programming === "cSharp") {
+              cSharp = cSharp + 1
+            } else if (programming === "java") {
+              javaScript = javaScript + 1
+            } else if (programming === "python") {
+              python = python + 1
+            }
+            console.log(python)
+            console.log(javaScript)
+            console.log(cSharp)
+            $(".questionFour").toggle();
+            $(".questionFive").toggle();
+            
+  
+          });
+
+          $("#submitFive").click(function() {
+            event.preventDefault();
+              const programming = $("input:radio[name=programming]:checked").val();
+              
+              if (programming === "cSharp") {
+                cSharp = cSharp + 1
+              } else if (programming === "java") {
+                javaScript = javaScript + 1
+              } else if (programming === "python") {
+                python = python + 1
+              }
+              console.log(python)
+              console.log(javaScript)
+              console.log(cSharp)
+              $(".questionFive").toggle();
+              $("#submitFinal").toggle();
+    
+          });
+
+          $("#submitFinal").click(function() {
+            event.preventDefault();
+            let maximum
+            if ((python > javaScript) && (python > cSharp)) {
+              $(".tryPython").toggle();
+            } else if ((javaScript > python) && (javaScript > cSharp)) {
+              $(".tryJavaScript").toggle();
+            } else if ((cSharp > javaScript) && (cSharp > python)) {
+              $(".tryCSharp").toggle();
+            } else {
+              $(".tie").toggle();
+            }
+
+
+          });
+          $("#restart").click(function() {
+
+          });
+
+
   });
