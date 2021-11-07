562622da8c6fb6d15d10167c7e7ff422ae92f278
bclauson <clauson.branden@gmail.com>
2021-11-06 22:59:14 -0700

    added html and scripts for question 3

--- js/scripts.js
+++ js/scripts.js
@@ -19,5 +19,46 @@ $(document).ready(function() {
       console.log(javaScript)
       console.log(cSharp)
       $(".questionOne").toggle();
+      $(".questionTwo").toggle();
     });
+    $("#submitTwo").click(function() {
+      event.preventDefault();
+        const programming = $("input:radio[name=programming]:checked").val();
+        
+        if (programming === "java") {
+          javaScript = javaScript + 1
+        } else if (programming === "python") {
+          python = python + 1
+        } else if (programming === "cSharp") {
+          cSharp = cSharp + 1
+        } else if (programming === "java") {
+          javaScript = javaScript + 1
+        }
+        console.log(python)
+        console.log(javaScript)
+        console.log(cSharp)
+        $(".questionTwo").toggle();
+        $(".questionThree").toggle();
+
+      });
+
+      $("#submitThree").click(function() {
+        event.preventDefault();
+          const programming = $("input:radio[name=programming]:checked").val();
+          
+          if (programming === "cSharp") {
+            cSharp = cSharp + 1
+          } else if (programming === "java") {
+            javaScript = javaScript + 1
+          } else if (programming === "python") {
+            python = python + 1
+          } else if (programming === "cSharp") {
+            cSharp = cSharp + 1
+          }
+          console.log(python)
+          console.log(javaScript)
+          console.log(cSharp)
+          $(".questionThree").toggle();
+          
+        });
   });
