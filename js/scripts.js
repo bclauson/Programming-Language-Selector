ceb5a7f5d1dfe74d6ba21f8800b1d92928cc9b51
bclauson <clauson.branden@gmail.com>
2021-11-06 17:24:11 -0700

    add scripts to tally which question has been selected

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,22 @@
 $(document).ready(function() {
-  $(".radio").click(function() {
-      const animal = $("input:radio[name=animal]:checked").val();
-      let result 
-      if (animal === "blueWhale") {
-        $(".blueWhale").toggle();
-      } else if (animal === "Clownfish") {
-        $(".clownfish").toggle();
-      } else if (animal === "Dolphin") {
-        $(".dolphin").toggle();
+  let python = 1
+  let cSharp = 1
+  let javaScript = 1
+  $(".questionOne").click(function() {
+      const programming = $("input:radio[name=programming]:checked").val();
+      
+      if (programming === "python") {
+        python = python + 1
+      } else if (programming === "cSharp") {
+        cSharp = cSharp + 1
+      } else if (programming === "java") {
+        javaScript = javaScript + 1
+      } else if (programming === "python") {
+        python = python + 1
       }
+      console.log(python)
+      console.log(javaScript)
+      console.log(cSharp)
+      $(".questionOne").toggle();
     });
   });
