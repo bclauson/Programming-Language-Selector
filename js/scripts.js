4bc6f4ba429a40c7048dc5f98cd578b124067321
bclauson <clauson.branden@gmail.com>
2021-11-09 15:08:18 -0800

    cleaned up indentation and spacing

--- js/scripts.js
+++ js/scripts.js
@@ -4,106 +4,106 @@ $(document).ready(function() {
   let javaScript = 1
   $("#submitOne").click(function() {
     event.preventDefault();
-      const programming = $("input:radio[name=programming]:checked").val();
+    const programming = $("input:radio[name=programming]:checked").val();
       
-      if (programming === "python") {
-        python = python + 1
-      } else if (programming === "cSharp") {
-        cSharp = cSharp + 1
-      } else if (programming === "java") {
-        javaScript = javaScript + 1
-      }
+    if (programming === "python") {
+      python = python + 1
+    } else if (programming === "cSharp") {
+      cSharp = cSharp + 1
+    } else if (programming === "java") {
+      javaScript = javaScript + 1
+    }
 
-      $(".questionOne").toggle();
-      $(".questionTwo").toggle();
-    });
-    $("#submitTwo").click(function() {
-      event.preventDefault();
-        const programming = $("input:radio[name=programming]:checked").val();
-        
-        if (programming === "java") {
-          javaScript = javaScript + 1
-        } else if (programming === "python") {
-          python = python + 1
-        } else if (programming === "cSharp") {
-          cSharp = cSharp + 1
-        } 
-
-        $(".questionTwo").toggle();
-        $(".questionThree").toggle();
-
-      });
-
-      $("#submitThree").click(function() {
-        event.preventDefault();
-          const programming = $("input:radio[name=programming]:checked").val();
-          
-          if (programming === "cSharp") {
-            cSharp = cSharp + 1
-          } else if (programming === "java") {
-            javaScript = javaScript + 1
-          } else if (programming === "python") {
-            python = python + 1
-          }
-
-          $(".questionThree").toggle();
-          $(".questionFour").toggle();
-
-        });
-
-        $("#submitFour").click(function() {
-          event.preventDefault();
-            const programming = $("input:radio[name=programming]:checked").val();
-            
-            if (programming === "cSharp") {
-              cSharp = cSharp + 1
-            } else if (programming === "java") {
-              javaScript = javaScript + 1
-            } else if (programming === "python") {
-              python = python + 1
-            }
-
-            $(".questionFour").toggle();
-            $(".questionFive").toggle();
-            
-  
-          });
-
-          $("#submitFive").click(function() {
-            event.preventDefault();
-              const programming = $("input:radio[name=programming]:checked").val();
-              
-              if (programming === "cSharp") {
-                cSharp = cSharp + 1
-              } else if (programming === "java") {
-                javaScript = javaScript + 1
-              } else if (programming === "python") {
-                python = python + 1
-              }
-
-              $(".questionFive").toggle();
-              $("#submitFinal").toggle();
+    $(".questionOne").toggle();
+    $(".questionTwo").toggle();
+  });
     
-          });
+  $("#submitTwo").click(function() {
+    event.preventDefault();
+    const programming = $("input:radio[name=programming]:checked").val();
+    
+    if (programming === "java") {
+      javaScript = javaScript + 1
+    } else if (programming === "python") {
+      python = python + 1
+    } else if (programming === "cSharp") {
+      cSharp = cSharp + 1
+    } 
 
-          $("#submitFinal").click(function() {
-            event.preventDefault();
-            let maximum
-            if ((python > javaScript) && (python > cSharp)) {
-              $(".tryPython").toggle();
-            } else if ((javaScript > python) && (javaScript > cSharp)) {
-              $(".tryJavaScript").toggle();
-            } else if ((cSharp > javaScript) && (cSharp > python)) {
-              $(".tryCSharp").toggle();
-            } else {
-              $(".tie").toggle();
-            }
+    $(".questionTwo").toggle();
+    $(".questionThree").toggle();
 
+  });
 
-          });
-          $("#restart").click(function() {
+  $("#submitThree").click(function() {
+    event.preventDefault();
+    const programming = $("input:radio[name=programming]:checked").val();
+    
+    if (programming === "cSharp") {
+      cSharp = cSharp + 1
+    } else if (programming === "java") {
+      javaScript = javaScript + 1
+    } else if (programming === "python") {
+      python = python + 1
+    }
 
-          });
+    $(".questionThree").toggle();
+    $(".questionFour").toggle();
+
+  });
+
+  $("#submitFour").click(function() {
+    event.preventDefault();
+    const programming = $("input:radio[name=programming]:checked").val();
+    
+    if (programming === "cSharp") {
+      cSharp = cSharp + 1
+    } else if (programming === "java") {
+      javaScript = javaScript + 1
+    } else if (programming === "python") {
+      python = python + 1
+    }
+
+    $(".questionFour").toggle();
+    $(".questionFive").toggle();
+
+   });
+
+  $("#submitFive").click(function() {
+    event.preventDefault();
+    const programming = $("input:radio[name=programming]:checked").val();
+    
+    if (programming === "cSharp") {
+      cSharp = cSharp + 1
+    } else if (programming === "java") {
+      javaScript = javaScript + 1
+    } else if (programming === "python") {
+      python = python + 1
+    }
+
+    $(".questionFive").toggle();
+    $("#submitFinal").toggle();
+    
+  });
+
+  $("#submitFinal").click(function() {
+    event.preventDefault();
+    let maximum
+    if ((python > javaScript) && (python > cSharp)) {
+      $(".tryPython").toggle();
+    } else if ((javaScript > python) && (javaScript > cSharp)) {
+      $(".tryJavaScript").toggle();
+    } else if ((cSharp > javaScript) && (cSharp > python)) {
+      $(".tryCSharp").toggle();
+    } else {
+      $(".tie").toggle();
+    }
 
 
   });
+  $("#restart").click(function() {
+
+  });
+
+
+});
